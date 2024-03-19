b50ecfd3f0a6506108fb57519b8961d92170b6ac
HMAsp <hanand55935@stud.noroff.no>
2024-03-19 08:46:34 +0100

    Add hamburger menu to header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,25 @@
 import Nav from "../Nav";
 import SearchField from "../Search";
 import CartIcon from "../CartIcon";
+import Hamburger from "hamburger-react";
+import { useState } from "react";
 
 const Header = () => {
+  const [isOpen, setOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setOpen(!isOpen);
+  };
+
   return (
     <header>
-      <Nav />
-      <SearchField />
-      <CartIcon />
+      <Hamburger onClick={toggleMenu} />
+
+      <div className={isOpen ? "menu open" : "menu"}>
+        <Nav />
+        <SearchField />
+        <CartIcon />
+      </div>
     </header>
   );
 };
